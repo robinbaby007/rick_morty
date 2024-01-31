b8264a14587c0e8f901053665d04cc7d8208ca0c
Robin Baby <robin.baby@qburst.com>
2024-01-31 17:33:43 +0530

    Add CharacterState and api called

--- app/src/main/java/com/example/rickmorty/presentation/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/rickmorty/presentation/viewModels/MainViewModel.kt
@@ -1,10 +1,36 @@
 package com.example.rickmorty.presentation.viewModels
 
 import androidx.lifecycle.ViewModel
-import com.example.network.KtorClient
+import androidx.lifecycle.viewModelScope
+import com.example.network.Character
 import com.example.rickmorty.repositories.CharacterRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(private val characterRepository: CharacterRepository): ViewModel(){
+@HiltViewModel
+class MainViewModel @Inject constructor(private val characterRepository: CharacterRepository) :
+    ViewModel() {
+    private var _characterDetailsState = MutableStateFlow<CharacterState>(CharacterState.Loading)
+    val characterDetailsState = _characterDetailsState.asStateFlow()
 
+    init {
+        viewModelScope.launch {
+            val characterDetails = characterRepository
+                .getCharacter(1)
+            _characterDetailsState
+                .update {
+                    CharacterState.Success(character = characterDetails)
+                }
+        }
+    }
+}
+
+sealed interface CharacterState {
+    data object Loading : CharacterState
+    data class Success(val character: Character) : CharacterState
+    data class Error(val message: String) : CharacterState
 }
