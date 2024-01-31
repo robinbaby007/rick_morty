0c9e451da9b6f2c8187f8506c74da929e4128b97
Robin Baby <robin.baby@qburst.com>
2024-01-31 20:32:50 +0530

    Add ApiOperation to handle network error and result

--- network/src/main/java/com/example/network/KtorClient.kt
+++ network/src/main/java/com/example/network/KtorClient.kt
@@ -36,12 +36,10 @@ class KtorClient {
 sealed interface ApiOperation<T> {
     data class Success<T>(val data: T) : ApiOperation<T>
     data class Failure<T>(val exception: Exception) : ApiOperation<T>
-
     fun onSuccess(block: (T) -> Unit): ApiOperation<T> {
         if (this is Success) block(data)
         return this
     }
-
     fun onFailure(block: (Exception) -> Unit): ApiOperation<T> {
         if (this is Failure) block(exception)
         return this
