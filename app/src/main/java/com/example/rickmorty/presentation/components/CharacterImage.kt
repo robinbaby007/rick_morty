d43e46690bc37375d6d2a477407b016765eaf0d7
Robin Baby <robin.baby@qburst.com>
2024-01-31 18:32:54 +0530

    Add CharacterImage composable

--- app/src/main/java/com/example/rickmorty/presentation/components/CharacterImage.kt
+++ app/src/main/java/com/example/rickmorty/presentation/components/CharacterImage.kt
@@ -1,4 +1,27 @@
 package com.example.rickmorty.presentation.components
 
-class CharacterImage {
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import com.example.rickmorty.presentation.components.common.LoadingState
+
+@Composable
+fun CharacterImage(
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    SubcomposeAsyncImage(
+        model = imageUrl,
+        contentDescription = "Image Item",
+        contentScale = ContentScale.Crop,
+        loading = {
+            LoadingState()
+        },
+        modifier = modifier.clip(RoundedCornerShape(12.dp))
+    )
 }
