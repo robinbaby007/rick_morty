fcaf013d4c9d58a90c887b2d8ba15473ff46a16d
Robin Baby <robin.baby@qburst.com>
2024-01-31 18:13:24 +0530

    Add CharacterState in CharacterDetailsScreen

--- app/src/main/java/com/example/rickmorty/presentation/components/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/rickmorty/presentation/components/CharacterDetailsScreen.kt
@@ -1,14 +1,32 @@
 package com.example.rickmorty.presentation.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.rickmorty.presentation.viewModels.MainViewModel
+import com.example.rickmorty.presentation.viewModels.CharacterState
+import com.example.rickmorty.presentation.viewModels.CharacterViewModel
 
 @Composable
 fun CharacterDetailsScreen(
     characterId: Int,
-    viewModel: MainViewModel = hiltViewModel()
+    characterViewModel: CharacterViewModel = hiltViewModel()
 ) {
+    val characterDetailsState = characterViewModel.characterDetailsState.collectAsState()
+    LaunchedEffect(key1 = Unit, block = {
+        characterViewModel.getCharacterDetails(characterId = characterId)
+    })
+    when (characterDetailsState.value) {
+        is CharacterState.Loading -> {
 
+        }
 
+        is CharacterState.Success -> {
+
+        }
+
+        is CharacterState.Error -> {
+
+        }
+    }
 }
