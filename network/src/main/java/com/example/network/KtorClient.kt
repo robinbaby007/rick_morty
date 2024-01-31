6eb846def44c114990481d035448cb3bcdc05c1f
Robin Baby <robin.baby@qburst.com>
2024-01-31 14:48:59 +0530

    Add getCharacter function to get character details using id

--- network/src/main/java/com/example/network/KtorClient.kt
+++ network/src/main/java/com/example/network/KtorClient.kt
@@ -1,12 +1,14 @@
 package com.example.network
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -25,4 +27,8 @@ class KtorClient {
             })
         }
     }
+
+    suspend fun getCharacter(id: Int) = client
+        .get("character/$id")
+        .body<Character>()
 }
