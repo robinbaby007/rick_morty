fda1a6bbd70d61f1c385d81b4207a95ab365fec7
Robin Baby <robin.baby@qburst.com>
2024-01-31 19:04:17 +0530

    Bug fix viewmodel name

--- app/src/main/java/com/example/rickmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickmorty/MainActivity.kt
@@ -3,26 +3,19 @@ package com.example.rickmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.rickmorty.presentation.components.CharacterDetailsScreen
-import com.example.rickmorty.presentation.viewModels.MainViewModel
 import com.example.rickmorty.ui.theme.RickMortyTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.random.Random
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val mainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,7 +29,7 @@ class MainActivity : ComponentActivity() {
                         navController = navController,
                         startDestination = "characterDetails"
                     ) {
-                        composable(route = "character_details") {
+                        composable(route = "characterDetails") {
                             CharacterDetailsScreen((1..10).random())
                         }
                     }
