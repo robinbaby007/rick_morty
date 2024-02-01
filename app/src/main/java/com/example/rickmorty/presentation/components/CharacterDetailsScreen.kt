739fef63fd210b0b9b62c35a52450d99fc9c881c
Robin Baby <robin.baby@qburst.com>
2024-02-01 17:14:33 +0530

    CharacterDetailedScreen edited

--- app/src/main/java/com/example/rickmorty/presentation/components/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/rickmorty/presentation/components/CharacterDetailsScreen.kt
@@ -1,10 +1,15 @@
 package com.example.rickmorty.presentation.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,7 +20,11 @@ import com.example.rickmorty.presentation.viewModels.CharacterViewModel
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.rickmorty.presentation.components.common.HeadingValue
 
 @Composable
 fun CharacterDetailsScreen(
@@ -40,13 +49,35 @@ fun CharacterDetailsScreen(
         }
 
         is CharacterState.Success -> {
-            LazyColumn {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+            ) {
+
+
+                items(state.dataPoints) { dataPoint ->
+                    HeadingValue(item = dataPoint)
+                }
+
+                item { Spacer(modifier = Modifier.height(8.dp)) }
+
                 item {
                     CharacterImage(state.character.image)
                 }
                 item { Spacer(modifier = Modifier.height(8.dp)) }
                 item {
-                    Text(text = state.character.name)
+                    Text(
+                        text = "View All Episodes",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .border(
+                                shape = RoundedCornerShape(10.dp),
+                                color = Color.Yellow,
+                                width = 1.dp
+                            )
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
                 }
             }
         }
@@ -57,7 +88,7 @@ fun CharacterDetailsScreen(
                     .fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                Text(text =state.message)
+                Text(text = state.message)
             }
         }
     }
