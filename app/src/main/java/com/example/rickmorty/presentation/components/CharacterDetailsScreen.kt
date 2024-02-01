d0788e84e9002e2e3ba37876ebee95d320cda548
Robin Baby <robin.baby@qburst.com>
2024-02-01 17:15:09 +0530

    CharacterDetailedScreen edited

--- app/src/main/java/com/example/rickmorty/presentation/components/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/rickmorty/presentation/components/CharacterDetailsScreen.kt
@@ -20,7 +20,6 @@ import com.example.rickmorty.presentation.viewModels.CharacterViewModel
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
