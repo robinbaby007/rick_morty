0d1947866a19748f668e633f3cb864de08ca3a3a
Robin Baby <robin.baby@qburst.com>
2024-02-01 20:59:04 +0530

    Add safecall in ktorclient

--- network/src/main/java/com/example/network/KtorClient.kt
+++ network/src/main/java/com/example/network/KtorClient.kt
@@ -1,5 +1,6 @@
 package com.example.network
 
+import com.example.network.models.remote.Character
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
