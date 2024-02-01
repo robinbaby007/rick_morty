25edf77b23c0c509040596d95b4f1100c423f3cd
Robin Baby <robin.baby@qburst.com>
2024-02-01 12:38:01 +0530

    getcharacter function wrapped with ApiOperation

--- app/src/main/java/com/example/rickmorty/repositories/CharacterRepository.kt
+++ app/src/main/java/com/example/rickmorty/repositories/CharacterRepository.kt
@@ -1,9 +1,10 @@
 package com.example.rickmorty.repositories
 
+import com.example.network.ApiOperation
 import com.example.network.Character
 import com.example.network.KtorClient
 import javax.inject.Inject
 
 class CharacterRepository @Inject constructor(private val ktorClient: KtorClient) {
-    suspend fun getCharacter(id: Int): Character = ktorClient.getCharacter(id = id)
+    suspend fun getCharacter(id: Int): ApiOperation<Character> = ktorClient.getCharacter(id = id)
 }
