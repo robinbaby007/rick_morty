2f93fa9c44511562d52dd5cf490c92f3caa0d69e
Robin Baby <robin.baby@qburst.com>
2024-02-02 15:20:36 +0530

    Renamed Character to RemoteCharacter

--- network/src/main/java/com/example/network/models/remote/RemoteCharacter.kt
+++ network/src/main/java/com/example/network/models/remote/RemoteCharacter.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Character(
+data class RemoteCharacter(
     @SerialName("created")
     val created: String,
     @SerialName("episode")
