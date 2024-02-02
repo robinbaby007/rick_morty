b03a5b4b32da08e2aede806eec15c96b8464ef49
Robin Baby <robin.baby@qburst.com>
2024-02-02 15:37:17 +0530

    Changed RemoteCharacter

--- network/src/main/java/com/example/network/models/domain/DomainCharacter.kt
+++ network/src/main/java/com/example/network/models/domain/DomainCharacter.kt
@@ -1,48 +1,27 @@
 package com.example.network.models.domain
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class DomainCharacter(
-    @SerialName("created")
     val created: String,
-    @SerialName("episode")
     val episode: List<String>,
-    @SerialName("gender")
-    val gender: String,
-    @SerialName("id")
+    val gender: CharacterGender,
     val id: Int,
-    @SerialName("image")
     val image: String,
-    @SerialName("location")
     val location: Location,
-    @SerialName("name")
     val name: String,
-    @SerialName("origin")
     val origin: Origin,
-    @SerialName("species")
     val species: String,
-    @SerialName("status")
-    val status: String,
-    @SerialName("type")
+    val status: CharacterStatus,
     val type: String,
-    @SerialName("url")
     val url: String
-){
-    @Serializable
+) {
     data class Location(
-        @SerialName("name")
         val name: String,
-        @SerialName("url")
         val url: String
     )
 
-    @Serializable
     data class Origin(
-        @SerialName("name")
         val name: String,
-        @SerialName("url")
         val url: String
     )
 }
