e881ac9321d7e9ab4737e7f8ac40c78e228fcc85
Robin Baby <robin.baby@qburst.com>
2024-02-02 15:19:55 +0530

    Add CharacterGender

--- network/src/main/java/com/example/network/models/domain/CharacterGender.kt
+++ network/src/main/java/com/example/network/models/domain/CharacterGender.kt
@@ -1,4 +1,9 @@
 package com.example.network.models.domain
 
-class CharacterGender {
+sealed class CharacterGender(val gender: String) {
+    data object Male : CharacterGender("Male")
+    data object Female : CharacterGender("Female")
+    data object GenderLess : CharacterGender("GenderLess")
+    data object Unknown : CharacterGender("Unknown")
+
 }
