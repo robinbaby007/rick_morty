f0f371caa4e3cf752365a84c2cd7e639109dc8c3
Robin Baby <robin.baby@qburst.com>
2024-02-02 15:21:01 +0530

    Add CharacterStatus

--- network/src/main/java/com/example/network/models/domain/CharacterStatus.kt
+++ network/src/main/java/com/example/network/models/domain/CharacterStatus.kt
@@ -1,4 +1,10 @@
 package com.example.network.models.domain
 
-class CharacterStatus {
+import android.graphics.Color
+
+
+sealed class CharacterStatus(status: String, color: Int) {
+    data object Alive : CharacterStatus("Alive", Color.GREEN)
+    data object Dead : CharacterStatus("Dead", Color.RED)
+    data object Unknown : CharacterStatus("Unknown", Color.YELLOW)
 }
